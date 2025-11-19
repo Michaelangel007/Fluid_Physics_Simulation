bd994075b7704f2ba7d73472adbd5a62f25c106a
michaelangel007 <michaelangel007@sharedcraft.com>
2025-11-18 19:32:29 -0800

    Version 1.6

--- Fluid_Physics_Simulation/src/Main.cpp
+++ Fluid_Physics_Simulation/src/Main.cpp
@@ -7,7 +7,7 @@
 #include <limits> // MAX_INT
 
        const char  *APP_NAME     = "Fluid Physics Simulation";
-static const char  *APP_VERSION  = "Version 1.5";
+static const char  *APP_VERSION  = "Version 1.6";
 
 // Configuration
 static float  g_nAspectRatio        = 1.0f;
